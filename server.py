d8d9371e1c00cf690a2a8a1864554abd362a4439
Cher Lafferty <cherlafferty01@gmail.com>
2022-01-15 19:49:34 -0800

    second

--- server.py
+++ server.py
@@ -4,12 +4,23 @@ app.secret_key = 'just@test'
 
 @app.route('/')
 def index():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] += 1
     return render_template("index.html")
 
-@app.route('/count', methods=['POST'])
-def count():
-    print("got this far")
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
+# Note: did not use methods=['Post'], why?
+# @app.route('/reset', methods=['POST'])
+# def reset():
+#     session.clear()
+#     return redirect('/')
